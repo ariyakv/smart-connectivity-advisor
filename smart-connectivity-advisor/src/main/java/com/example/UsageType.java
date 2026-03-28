d315cfce05a922ba88be7d4fe2628a51d3509278
Ariya Kasi Viswanathan <ariyakv@Ariyas-MacBook-Air.local>
2026-03-28 13:00:17 -0400

    UsageType.java update

--- smart-connectivity-advisor/src/main/java/com/example/UsageType.java
+++ smart-connectivity-advisor/src/main/java/com/example/UsageType.java
@@ -1,5 +1,11 @@
 package com.example;
 
-public class UsageType {
-
-}
+/**
+ * Represents the different ways a user may use their internet connection.
+ *
+ * These usage types help the recommendation system determine how much
+ * bandwidth and reliability a household needs.
+ */
+public enum UsageType {
+    SCHOOL, REMOTE_WORK, STREAMING, GAMING, BROWSING
+} //UsageType
