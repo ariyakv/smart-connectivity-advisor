bca40e13a61bf6e9317eca1b2aa45f322dc1f24a
Ariya Kasi Viswanathan <ariyakv@Ariyas-MacBook-Air.local>
2026-03-28 12:04:49 -0400

    Test Run/src/App.java

--- Test Run/src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println(App.stringCount("Ariya"));
-    }
-
-    public static int stringCount(String name) {
-        return name.length();
-    }
-}
