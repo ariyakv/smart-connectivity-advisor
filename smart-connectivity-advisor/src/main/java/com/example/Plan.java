3f7ba5f33bcc39435d13767767be9acdb5ff9878
Ariya Kasi Viswanathan <ariyakv@Ariyas-MacBook-Air.local>
2026-03-28 12:51:29 -0400

    Plan update

--- smart-connectivity-advisor/src/main/java/com/example/Plan.java
+++ smart-connectivity-advisor/src/main/java/com/example/Plan.java
@@ -1,5 +1,60 @@
 package com.example;
 
 public class Plan {
+    private String name;
+    private int monthlyPrice;
+    private int speedMbps;
+    private boolean mobileBackupIncluded;
+    
+    /**
+     * Constructor for Plan. Includes name, monthlyPrice, speedMbps, and 
+     * if mobile backup is included or not.
+     * @param name
+     * @param monthlyPrice
+     * @param speedMbps
+     * @param mobileBackupIncluded
+     */
+    public Plan(String name, int monthlyPrice, int speedMbps, boolean mobileBackupIncluded) {
+        this.name = name;
+        this.monthlyPrice = monthlyPrice;
+        this.speedMbps = speedMbps;
+        this.mobileBackupIncluded = mobileBackupIncluded;
+    } //Plan
+    /** Returns this name
+     * @return this.name
+     */
+    public String getName() {
+        return name;
+    } //getName
 
-}
+    /**
+     * Returns monthly price.
+     * @return this.monthlyPrice.
+     */
+    public int getMonthlyPrice() {
+        return monthlyPrice;
+    } //getMonthlyPrice
+
+    /**
+     * Returns mobileBackupIncluded
+     * @return this.mobileBackupIncluded
+     */
+    public boolean isMobileBackupIncluded() {
+        return mobileBackupIncluded;
+    } //isMobileBackupIncludes
+
+    /**
+     * Returns this.speedMbps.
+     * @return this.speedMbps.
+     */
+    public int getSpeedMbps() {
+        return speedMbps;
+    } //getSpeedMbps
+    /**
+     * Return string representation of all instance variables using SOUT.
+     */
+    @Override
+    public String toString() {
+        return name + "($" + monthlyPrice + "/mo, " + speedMbps + " Mbps)"; 
+    }//toString
+}//Plan
