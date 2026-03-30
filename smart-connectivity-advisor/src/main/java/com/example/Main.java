27af0dda28f8f8e7a1bada5b588f7795c6f98769
Ariya Kasi Viswanathan <ariyakv@Ariyas-MacBook-Air.local>
2026-03-30 19:52:32 -0400

    new changes to work with java 25

--- smart-connectivity-advisor/src/main/java/com/example/Main.java
+++ smart-connectivity-advisor/src/main/java/com/example/Main.java
@@ -20,7 +20,7 @@ import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
-//import javafx.scene.layout.Region;
+
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
